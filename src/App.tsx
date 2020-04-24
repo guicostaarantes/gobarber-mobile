c28ae94cd8463970b312bcf6981a09c92699e2c5
guicostaarantes <arantes.g@outlook.com>
2020-04-24 08:52:55 -0300

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 
+import { NavigationContainer } from '@react-navigation/native';
+
+import { StatusBar } from 'react-native';
+
+import { AuthRoutes } from './routes';
+
 const App: React.FC = () => {
-  return <h1>App</h1>;
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <AuthRoutes />
+    </NavigationContainer>
+  );
 };
 
 export default App;
