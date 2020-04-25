0b048af48dab292536970350823ddde13dcd1360
guicostaarantes <arantes.g@outlook.com>
2020-04-24 23:47:24 -0300

    added signin screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  ForgotContainer,
+  ForgotText,
+  SignUpContainer,
+  SignUpText,
+} from './styles';
+import Input from '../../components/input';
+import Button from '../../components/button';
 
 import logoImg from '../../assets/logo.png';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-    </Container>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={{ flex: 1 }}
+    >
+      <ScrollView contentContainerStyle={{ flex: 1 }}>
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+          <Input name="email" icon="mail" placeholder="Email" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+          <Button onPress={() => console.log('')}>Entrar</Button>
+          <ForgotContainer>
+            <ForgotText>Esqueci minha senha</ForgotText>
+          </ForgotContainer>
+        </Container>
+      </ScrollView>
+      <SignUpContainer>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <SignUpText>Criar nova conta</SignUpText>
+      </SignUpContainer>
+    </KeyboardAvoidingView>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.KeyboardAvoidingView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  color: #f4ede8;
+  margin: 64px 0 24px 0;
+`;
+
+export const ForgotContainer = styled.TouchableOpacity`
+  margin: 24px;
+`;
+
+export const ForgotText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: #f4ede8;
+`;
+
+export const SignUpContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-top-width: 1px;
+  border-top-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px 0;
+  align-items: center;
+  justify-content: center;
+  background: #312e38;
+`;
+
+export const SignUpText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: #ff9000;
+  margin-left: 10px;
+`;
