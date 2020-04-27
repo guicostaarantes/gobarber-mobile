2da6368fd0dd474e55a48910c7b4e9fd1b7d0692
guicostaarantes <arantes.g@outlook.com>
2020-04-26 22:03:32 -0300

    added scrollview to screens

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useCallback, useRef } from 'react';
 
-import { Image, KeyboardAvoidingView, Platform, View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
 
@@ -8,6 +16,8 @@ import { useNavigation } from '@react-navigation/native';
 
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
+
+import { useKeyboard } from '@react-native-community/hooks';
 
 import {
   Container,
@@ -21,10 +31,10 @@ import Input from '../../components/input';
 import Button from '../../components/button';
 
 import logoImg from '../../assets/logo.png';
-import { ScrollView } from 'react-native-gesture-handler';
 
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
+  const keyboard = useKeyboard();
 
   const formRef = useRef<FormHandles>(null);
   const handleSubmit = useCallback((data) => {
@@ -32,41 +42,51 @@ const SignIn: React.FC = () => {
   }, []);
 
   return (
-    <KeyboardAvoidingView behavior="height" style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={{ flex: 1 }}>
-        <Container>
-          <Image source={logoImg} />
-          <View>
-            <Title>Faça seu logon</Title>
-          </View>
-          <Form ref={formRef} onSubmit={handleSubmit}>
-            <Input
-              name="email"
-              icon="mail"
-              placeholder="Email"
-              autoCorrect={false}
-              autoCapitalize="none"
-              keyboardType="email-address"
-            />
-            <Input
-              name="password"
-              icon="lock"
-              placeholder="Senha"
-              secureTextEntry={true}
-            />
-            <Button onPress={() => formRef.current?.submitForm()}>
-              Entrar
-            </Button>
-          </Form>
-          <ForgotContainer>
-            <ForgotText>Esqueci minha senha</ForgotText>
-          </ForgotContainer>
-        </Container>
-      </ScrollView>
-      <SignUpContainer onPress={() => navigation.navigate('sign-up')}>
-        <Icon name="log-in" size={20} color="#ff9000" />
-        <SignUpText>Criar nova conta</SignUpText>
-      </SignUpContainer>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          style={{ flexGrow: 1 }}
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input
+                name="email"
+                icon="mail"
+                placeholder="Email"
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+              />
+              <Input
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry={true}
+              />
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
+            <ForgotContainer>
+              <ForgotText>Esqueci minha senha</ForgotText>
+            </ForgotContainer>
+          </Container>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+      {!keyboard.keyboardShown && (
+        <SignUpContainer onPress={() => navigation.navigate('sign-up')}>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <SignUpText>Criar nova conta</SignUpText>
+        </SignUpContainer>
+      )}
     </KeyboardAvoidingView>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useRef, useCallback } from 'react';
 
-import { Image, KeyboardAvoidingView, View } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  View,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
 
@@ -9,15 +17,17 @@ import { useNavigation } from '@react-navigation/native';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
 
+import { useKeyboard } from '@react-native-community/hooks';
+
 import { Container, Title, BackContainer, BackText } from './styles';
 import Input from '../../components/input';
 import Button from '../../components/button';
 
 import logoImg from '../../assets/logo.png';
-import { ScrollView } from 'react-native-gesture-handler';
 
 const SignUp: React.FC = () => {
   const navigation = useNavigation();
+  const keyboard = useKeyboard();
 
   const formRef = useRef<FormHandles>(null);
   const handleSubmit = useCallback((data) => {
@@ -25,51 +35,61 @@ const SignUp: React.FC = () => {
   }, []);
 
   return (
-    <KeyboardAvoidingView behavior="height" style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={{ flex: 1 }}>
-        <Container>
-          <Image source={logoImg} />
-          <View>
-            <Title>Faça seu cadastro</Title>
-          </View>
-          <Form ref={formRef} onSubmit={handleSubmit}>
-            <Input
-              name="fullName"
-              icon="user"
-              placeholder="Nome"
-              autoCorrect={false}
-              autoCapitalize="words"
-            />
-            <Input
-              name="email"
-              icon="mail"
-              placeholder="Email"
-              autoCorrect={false}
-              autoCapitalize="none"
-              keyboardType="email-address"
-            />
-            <Input
-              name="password"
-              icon="lock"
-              placeholder="Senha"
-              secureTextEntry={true}
-            />
-            <Input
-              name="confirmPassword"
-              icon="lock"
-              placeholder="Confirmar Senha"
-              secureTextEntry={true}
-            />
-            <Button onPress={() => formRef.current?.submitForm()}>
-              Cadastrar
-            </Button>
-          </Form>
-        </Container>
-      </ScrollView>
-      <BackContainer onPress={() => navigation.goBack()}>
-        <Icon name="arrow-left" size={20} color="#ff9000" />
-        <BackText>Voltar para logon</BackText>
-      </BackContainer>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          style={{ flexGrow: 1 }}
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu cadastro</Title>
+            </View>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input
+                name="fullName"
+                icon="user"
+                placeholder="Nome"
+                autoCorrect={false}
+                autoCapitalize="words"
+              />
+              <Input
+                name="email"
+                icon="mail"
+                placeholder="Email"
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+              />
+              <Input
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry={true}
+              />
+              <Input
+                name="confirmPassword"
+                icon="lock"
+                placeholder="Confirmar Senha"
+                secureTextEntry={true}
+              />
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Cadastrar
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+      {!keyboard.keyboardShown && (
+        <BackContainer onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={20} color="#ff9000" />
+          <BackText>Voltar para logon</BackText>
+        </BackContainer>
+      )}
     </KeyboardAvoidingView>
   );
 };
